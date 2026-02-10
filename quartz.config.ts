f518aeae756afb312b5ee10395397c23ab5b0478
Martin Mijangos <martin@MacBook-Air-de-Martin.local>
2026-02-10 16:12:53 -0600

    Fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "martinmij.github.io/geometria-analitica-ii",
+    https://martinmij.github.io/geometria-analitica-ii/,
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
